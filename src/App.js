e9d6066b8461be13e4b898851cc834eb376aa871
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2021-12-14 15:03:46 +0530

    Initialized Alan AI button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = 'a2ce568569784bc0c0a838badbab8ff82e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey
+        })
+    }, []);
+
+    return (
+        <div>
+            <h1>Testing</h1>
+        </div>
+    )
 }
 
 export default App;
